9b6e5ddf14a93930fdc10baaf019d5a098712714
Maxime <maxime.pouzargue7@gmail.com>
2023-09-26 17:53:17 +0200

    V2

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
-    # print(content) affiche toute la page html
-    
-    soup = BeautifulSoup(content, 'lxml')
-    # print(soup.prettify())
-    # courses_html_tags = soup.find_all('h5')
-    # # print(courses_html_tags) affiche les h5 avec les balises
-    # for course in courses_html_tags:
-    #     print(course.text) affiche le text des h5
-    course_cards = soup.find_all('div', class_="card")
-    for course in course_cards:
-        # print(course.h5)
-        course_name = course.h5.text
-        course_price = course.a.text.split()[-1]
+html_text = requests.get('https://www.artisans-du-batiment.com/trouver-un-artisan-qualifie/?job=Plombier&place=paris').text
 
-        print(f'{course_name} costs {course_price}')
+soup = BeautifulSoup(html_text, 'lxml')
+addresses = soup.find_all('div', class_='a-artisanTease__address')
 
-        # Fin 24:48 https://www.youtube.com/watch?v=XVv6mJpFOb0
+for address_div in addresses:
+    address_span = address_div.find('span')
+    if address_span:
+        print(address_span.text.strip())  # .strip() pour supprimer les espaces inutiles
