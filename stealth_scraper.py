4ce355186cdea896b182211001bd3e097df5c1f6
Lisiantys <maxime.pouzargue7@gmail.com>
2023-09-27 22:59:53 +0200

    V5 - Ajout chaine de caractère www + intervalles + sneaky

--- stealth_scraper.py
+++ stealth_scraper.py
@@ -1,3 +1,4 @@
+
 import time
 import os
 import random
@@ -9,27 +10,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
 # Function to introduce a random sleep to mimic human behavior
-def random_sleep(min_seconds=1, max_seconds=20):
+def random_sleep(min_seconds=1, max_seconds=3):
     time.sleep(random.uniform(min_seconds, max_seconds))
 
 # Setup Chrome options for stealth
 chrome_options = Options()
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 chrome_options.add_argument("--disable-images")
-chrome_options.add_argument("--start-maximized")  # Start browser maximized to look more like a real user
-chrome_options.add_argument("--disable-infobars") # Disable infobars
 
 # Start the Chrome driver with the modified options
 driver = webdriver.Chrome(options=chrome_options)
 print('Lancement de Chrome')
 
 driver.get('https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=clown&ou=Bretignolles+sur+Mer+%2885470%29&univers=pagesjaunes&idOu=L08503500')
-random_sleep(2, 5) # Let the user actually see something!
+random_sleep(2, 4)
 
 # Function to check if CAPTCHA is present on the page
 def is_captcha_present():
     try:
-        # This is just an example, you'd need to find the actual CAPTCHA element's selector
         driver.find_element(By.ID, "challenge-running")
         return True
     except NoSuchElementException:
@@ -38,15 +36,14 @@ def is_captcha_present():
 # Check for CAPTCHA and wait for user to solve it manually
 while is_captcha_present():
     print("CAPTCHA detected. Please solve it manually.")
-    random_sleep(15, 20)  # Check every 5 seconds
+    random_sleep(15, 20)
 
 print("CAPTCHA solved or not present.")
 driver.find_element(By.CLASS_NAME, "didomi-continue-without-agreeing").click()
-random_sleep(2, 5)
+random_sleep(2, 4)
 
 data = []
 print('Récupération des données en cours...')
-
 posts = driver.find_elements(By.CSS_SELECTOR, ".bi-generic")
 
 for post in posts:
@@ -54,21 +51,15 @@ for post in posts:
     adresse = post.find_element(By.CLASS_NAME, "bi-address").text
     secteur = post.find_element(By.CLASS_NAME, "bi-activity-unit").text
 
-    # Acces to the dedicate page
     lien_page_dediee = post.find_element(By.CLASS_NAME, "bi-denomination").get_attribute("href")
     post.find_element(By.CLASS_NAME, "bi-denomination").click()
-    random_sleep(2, 4)
+    random_sleep(3, 5)
 
+    try:
+        lien_site = driver.find_element(By.CSS_SELECTOR, ".lvs-container a").get_attribute("href")
+    except NoSuchElementException:
+        lien_site = None
 
-    try:  # IF there is a web site link / facebook link
-        lien_site = driver.find_element(By.CSS_SELECTOR, ".lvs-container a .value").text
-        if lien_site and not lien_site.startswith("www."):
-            lien_site = "www." + lien_site
-    except NoSuchElementException:  # If no link was found
-        lien_site = None  # Default value = None = No text in xml file
-
-
-    # Store the data
     data.append({
         "Secteur": secteur,
         "Nom": nom,
@@ -77,26 +68,18 @@ for post in posts:
         "Site Web": lien_site
     })
 
-    # Back to the previous page with list of posts
     driver.back()
-    random_sleep(1, 4)
+    random_sleep(1, 2)
 
-# Check if the Excel file already exists
 if os.path.exists("donnees_professionnels.xlsx"):
-    # Load existing data
     old_data = pd.read_excel("donnees_professionnels.xlsx", engine='openpyxl')
-    # Create a DataFrame from the new scraped data
     new_data = pd.DataFrame(data)
-    # Append new data to the old data
     df = pd.concat([old_data, new_data], ignore_index=True)
     print("Données ajoutées au fichier Xml...")
 else:
     print("Création d'un fichier XMLX en cours...")
-    # If the file does not exist, just create a DataFrame from the new scraped data
     df = pd.DataFrame(data)
-    
 
-# Save the DataFrame to an Excel file
 df.to_excel("donnees_professionnels.xlsx", index=False, engine='openpyxl')
 
 driver.quit()
