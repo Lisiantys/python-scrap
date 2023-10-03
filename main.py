37e81c2eafc3d735136930881ad173796df1c072
Maxime <maxime.pouzargue7@gmail.com>
2023-10-03 16:46:35 +0200

    V8.1 Scrap One page Ok

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
 import os
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-from utilities import random_sleep, start, error
+from utilities import random_sleep, start, smooth_scroll, close_popup, one_scroll_on_page
 
 print('Lancement de Chrome')
 
@@ -13,8 +12,8 @@ print('Lancement de Chrome')
 data = []
 
 driver = start()
-driver.get("https://www.paruvendu.fr/immobilier/location/appartement/")
-random_sleep(3, 4)
+driver.get("https://www.paruvendu.fr/immobilier/annonceimmofo/liste/listeAnnonces?tt=5&tbApp=1&tbDup=1&tbChb=1&tbLof=1&tbAtl=1&tbPla=1&at=1&pa=FR&lol=15&ddlFiltres=nofilter&codeINSEE=,33126,")
+random_sleep(2, 3)
 link_element = driver.find_element(By.XPATH, "//a[@onclick='cmp_pv.ui.showVendors()']")
 link_element.click()
 random_sleep(1, 2)
@@ -22,50 +21,65 @@ button = driver.find_element(By.XPATH, "//button[@onclick='cmp_pv.ui.switchAllPu
 button.click()
 random_sleep(2, 3)
 
-posts = driver.find_elements(By.CSS_SELECTOR, ".border-1.border-grey-75.shadow-xl.hover\\:shadow-2xl.bg-white.relative.p-4.sm\\:p-2.my-6")
+posts = driver.find_elements(By.CSS_SELECTOR, ".border-1.border-grey-75.shadow-xl.hover\\:shadow-2xl.bg-white.relative.p-4.sm\\:p-2")
 
 print('Récupération des données en cours...')
 
+initial_step = 150
+step_increment = 150
+
 for post in posts: 
-    random_sleep(2, 4)
-    # Supposons que `parent_element` est l'élément parent que vous avez déjà localisé
-    prix_div = post.find_element(By.CSS_SELECTOR, "div.flex.justify-center.items-center > div")
-    prix = prix_div.text.split()[0]  # Cela devrait vous donner "750 €"
-    print(prix)
 
-    # url = post.get_attribute('href')
+    url = post.find_element(By.CSS_SELECTOR, ".flex.sm\\:block.gap-4 a").get_attribute('href')
     random_sleep(0.5, 1)
-    # driver.get(url)
+    driver.get(url)
+    random_sleep(1, 2)
+    close_popup(driver)
+    random_sleep(1, 2)
+    one_scroll_on_page(driver)
+   
+    try:  
+        prix_element = driver.find_element(By.CSS_SELECTOR, "#autoprix")
+        prix = prix_element.text.split("\n")[0]
+    except NoSuchElementException: 
+        prix = None  
 
-    # try:  
-    #     # Localiser l'élément contenant l'e-mail
-    #     email_element = driver.find_element(By.XPATH, "//div[@class='col'][.//i[@class='fa fa-envelope']]")
+    try:  
+        titre = driver.find_element(By.CSS_SELECTOR, "#detail_h1").text
+    except NoSuchElementException: 
+        titre = None  
+    
+    try:  
+        lieu = driver.find_element(By.CSS_SELECTOR, "#detail_loc").text
+    except NoSuchElementException: 
+        lieu = None  
+    
+    try:  
+        sous_titre = driver.find_element(By.CSS_SELECTOR, ".autodetail-titre.sepdetail14-ssbordure").text
+    except NoSuchElementException: 
+        sous_titre = None  
 
-    #     # Extraire le texte de l'élément
-    #     email_text = email_element.text
+    try:  
+        description = driver.find_element(By.CSS_SELECTOR, "#txtAnnonceTrunc").text
+    except NoSuchElementException: 
+        description = None  
 
-    #     # Séparer le texte pour obtenir l'adresse e-mail
-    #     email = email_text.split(': ')[1]
+    random_sleep(1, 2)
 
-    #     print(email)  # Cela devrait afficher "reservations@hotel-omnubo.com"
+    driver.back()
+    random_sleep(2, 3)
+    smooth_scroll(driver, step=initial_step)
 
-    # except NoSuchElementException: 
-    #     email = None  
-
-    # try:  
-    #     # Localiser l'élément contenant l'e-mail
-    #     adress_element = driver.find_element(By.XPATH, "//div[@class='col'][.//i[@class='fa fa-map-marked']]")
-
-    #     # Extraire le texte de l'élément
-    #     adress = adress_element.text
-
-    #     print(adress)  # Cela devrait afficher "reservations@hotel-omnubo.com"
-
-    # except NoSuchElementException: 
-    #     adress = None  
+    # Incrémentez le step pour la prochaine itération de la boucle principale
+    initial_step += step_increment
 
     data.append({
+        "url": url,
         "prix": prix,
+        "titre": titre,
+        "sous-titre": sous_titre,
+        "lieu": lieu,
+        "description": description
     })
 
     random_sleep(0.5, 1) 
--- utilities.py
+++ utilities.py
@@ -2,17 +2,29 @@ import time
 import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36 Edg/91.0.864.59",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36 OPR/77.0.4054.203"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36 OPR/77.0.4054.203",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
 ]
 
 def random_sleep(min_seconds=1, max_seconds=5):
@@ -26,20 +38,39 @@ def start():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-def button_exists(driver):
+def smooth_scroll(driver, step=150, delay=1):
+    total_height = int(driver.execute_script("return document.body.scrollHeight"))
+    current_height = 0  # position actuelle de défilement
+    
+    last_height = -1  # pour stocker la hauteur précédente
+    max_iterations = 5  # pour éviter une boucle infinie
+    iterations = 0
+    
+    while current_height < total_height and iterations < max_iterations:
+        driver.execute_script(f"window.scrollBy(0, {step});")
+        time.sleep(delay)
+        
+        current_height += step
+        new_height = int(driver.execute_script("return document.body.scrollHeight"))
+
+        # Si la hauteur totale n'a pas changé, arrêtez de faire défiler
+        if new_height == last_height:
+            break
+        
+        last_height = new_height
+        iterations += 1
+
+def one_scroll_on_page(driver, pixels=500, delay=0.5):
+    driver.execute_script(f"window.scrollBy(0, {pixels});")
+
+def close_popup(driver):
     try:
-        # Attendez jusqu'à 5 secondes pour voir si le bouton est présent
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "reload-button")))
-        return True
-    except TimeoutException:
-        return False
-
-def error(driver):
-    while button_exists(driver):
-        random_sleep(1, 2)
-        button = driver.find_element(By.ID, "reload-button")
-        button.click()
-        random_sleep(1, 2)
-
-    random_sleep(1, 2)
-
+        close_button = WebDriverWait(driver, 4).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".fancybox-item.fancybox-close"))
+        )
+        # Vérifiez si l'élément est visible
+        if close_button.is_displayed():
+            close_button.click()
+            random_sleep(1, 2)  # Un petit délai après avoir fermé le pop-up
+    except (NoSuchElementException, TimeoutException):
+        pass  # Le pop-up n'était pas présent
