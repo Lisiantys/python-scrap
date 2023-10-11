2e363981d23e9f9b37a08c8c2a4fb58a7212b3d2
Maxime <maxime.pouzargue7@gmail.com>
2023-10-11 16:02:36 +0200

    Action chaine c'est enorme !!!

--- main.py
+++ main.py
@@ -2,89 +2,97 @@ import os
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
-from utilities import random_sleep, start, smooth_scroll, close_popup, one_scroll_on_page
+
+
+import time
+from utilities import random_sleep, start
 
 print('Lancement de Chrome')
 
 # lance le navigateur à la page dédiée
 
+def handle_modal(driver):
+    try:
+        # Attendre que le modal apparaisse
+        modal_button = WebDriverWait(driver, 4).until(
+            EC.element_to_be_clickable((By.ID, "tarteaucitronAllDenied2"))
+        )
+        modal_button.click()
+    except:
+        # Si le modal n'apparaît pas dans les 4 secondes, passez
+        pass
+
+
+
 data = []
+urls = [
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=1", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=2", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=3", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=4", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=5", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=6", 
+    "https://chambre-notaires-vendee.notaires.fr/annuaire-notaires?langs%5B0%5D=18&departments%5B0%5D=6&displayList=1&page=7"
+]
 
 driver = start()
-driver.get("https://www.paruvendu.fr/immobilier/annonceimmofo/liste/listeAnnonces?tt=5&tbApp=1&tbDup=1&tbChb=1&tbLof=1&tbAtl=1&tbPla=1&at=1&pa=FR&lol=15&ddlFiltres=nofilter&codeINSEE=,33126,")
-random_sleep(2, 3)
-link_element = driver.find_element(By.XPATH, "//a[@onclick='cmp_pv.ui.showVendors()']")
-link_element.click()
-random_sleep(1, 2)
-button = driver.find_element(By.XPATH, "//button[@onclick='cmp_pv.ui.switchAllPurposes(false);']")
-button.click()
-random_sleep(2, 3)
-
-posts = driver.find_elements(By.CSS_SELECTOR, ".border-1.border-grey-75.shadow-xl.hover\\:shadow-2xl.bg-white.relative.p-4.sm\\:p-2")
 
 print('Récupération des données en cours...')
 
-initial_step = 150
-step_increment = 150
+
+# for url in urls: 
+
+driver.get("https://www.udaf85.fr/les-associations-de-udaf/annuaire-des-associations/")
+random_sleep(1, 2)
+
+def scroll_incrementally(driver, increment=650, delay=0.5, times=8):
+    for _ in range(times):
+        driver.execute_script(f"window.scrollBy(0, {increment});")
+        time.sleep(delay)
+
+    # Appel à la méthode pour effectuer le défilement
+scroll_incrementally(driver)
+
+# Maintenant, récupérez tous les éléments
+posts = driver.find_elements(By.CSS_SELECTOR, "div.card--association-large")
+print('Nombre de posts trouvés:', len(posts))
+print('Récupération des données en cours...')
 
 for post in posts: 
+    try:
+        title = post.find_element(By.CSS_SELECTOR, ".card__title a").text
+    except NoSuchElementException:
+        title = None
+        
+    try:
+        adresse = post.find_element(By.CSS_SELECTOR, ".list-btn-contact__address span span").text
+    except NoSuchElementException:
+        adresse = None
 
-    url = post.find_element(By.CSS_SELECTOR, ".flex.sm\\:block.gap-4 a").get_attribute('href')
-    random_sleep(0.5, 1)
-    driver.get(url)
-    random_sleep(1, 2)
-    close_popup(driver)
-    random_sleep(1, 2)
-    one_scroll_on_page(driver)
-   
-    try:  
-        prix_element = driver.find_element(By.CSS_SELECTOR, "#autoprix")
-        prix = prix_element.text.split("\n")[0]
-    except NoSuchElementException: 
-        prix = None  
-
-    try:  
-        titre = driver.find_element(By.CSS_SELECTOR, "#detail_h1").text
-    except NoSuchElementException: 
-        titre = None  
-    
-    try:  
-        lieu = driver.find_element(By.CSS_SELECTOR, "#detail_loc").text
-    except NoSuchElementException: 
-        lieu = None  
-    
-    try:  
-        sous_titre = driver.find_element(By.CSS_SELECTOR, ".autodetail-titre.sepdetail14-ssbordure").text
-    except NoSuchElementException: 
-        sous_titre = None  
-
-    try:  
-        description = driver.find_element(By.CSS_SELECTOR, "#txtAnnonceTrunc").text
-    except NoSuchElementException: 
-        description = None  
-
-    random_sleep(1, 2)
-
-    driver.back()
-    random_sleep(2, 3)
-    smooth_scroll(driver, step=initial_step)
-
-    # Incrémentez le step pour la prochaine itération de la boucle principale
-    initial_step += step_increment
+    try:
+        email_button = post.find_element(By.CSS_SELECTOR, "a.btn-show-content.btn-email")
+        actions = ActionChains(driver)
+        actions.move_to_element(email_button).perform()  # This will scroll the view to the email button
+        email_button.click()
+        random_sleep(0.5, 0.8)
+        email = post.find_element(By.CSS_SELECTOR, "a[href^='mailto:']").get_attribute('href').replace('mailto:', '')
+        print(email)
+    except NoSuchElementException:
+        email = None
 
     data.append({
-        "url": url,
-        "prix": prix,
-        "titre": titre,
-        "sous-titre": sous_titre,
-        "lieu": lieu,
-        "description": description
+        "Nom": title,
+        "Adresse": adresse,
+        "email": email,
     })
 
     random_sleep(0.5, 1) 
 
-   
+
 # Check if the Excel file already exists
 if os.path.exists("donnees_professionnels.xlsx"):
     # Load existing data
--- utilities.py
+++ utilities.py
@@ -3,9 +3,6 @@ import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -27,7 +24,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
 ]
 
-def random_sleep(min_seconds=1, max_seconds=5):
+def random_sleep(min_seconds=0.1, max_seconds=10):
     time.sleep(random.uniform(min_seconds, max_seconds))
 
 def start():
@@ -38,39 +35,39 @@ def start():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-def smooth_scroll(driver, step=150, delay=1):
-    total_height = int(driver.execute_script("return document.body.scrollHeight"))
-    current_height = 0  # position actuelle de défilement
+# def smooth_scroll(driver, step=150, delay=1):
+#     total_height = int(driver.execute_script("return document.body.scrollHeight"))
+#     current_height = 0  # position actuelle de défilement
     
-    last_height = -1  # pour stocker la hauteur précédente
-    max_iterations = 5  # pour éviter une boucle infinie
-    iterations = 0
+#     last_height = -1  # pour stocker la hauteur précédente
+#     max_iterations = 5  # pour éviter une boucle infinie
+#     iterations = 0
     
-    while current_height < total_height and iterations < max_iterations:
-        driver.execute_script(f"window.scrollBy(0, {step});")
-        time.sleep(delay)
+#     while current_height < total_height and iterations < max_iterations:
+#         driver.execute_script(f"window.scrollBy(0, {step});")
+#         time.sleep(delay)
         
-        current_height += step
-        new_height = int(driver.execute_script("return document.body.scrollHeight"))
+#         current_height += step
+#         new_height = int(driver.execute_script("return document.body.scrollHeight"))
 
-        # Si la hauteur totale n'a pas changé, arrêtez de faire défiler
-        if new_height == last_height:
-            break
+#         # Si la hauteur totale n'a pas changé, arrêtez de faire défiler
+#         if new_height == last_height:
+#             break
         
-        last_height = new_height
-        iterations += 1
+#         last_height = new_height
+#         iterations += 1
 
-def one_scroll_on_page(driver, pixels=500, delay=0.5):
-    driver.execute_script(f"window.scrollBy(0, {pixels});")
+# def one_scroll_on_page(driver, pixels=500, delay=0.5):
+#     driver.execute_script(f"window.scrollBy(0, {pixels});")
 
-def close_popup(driver):
-    try:
-        close_button = WebDriverWait(driver, 4).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".fancybox-item.fancybox-close"))
-        )
-        # Vérifiez si l'élément est visible
-        if close_button.is_displayed():
-            close_button.click()
-            random_sleep(1, 2)  # Un petit délai après avoir fermé le pop-up
-    except (NoSuchElementException, TimeoutException):
-        pass  # Le pop-up n'était pas présent
+# def close_popup(driver):
+#     try:
+#         close_button = WebDriverWait(driver, 4).until(
+#             EC.element_to_be_clickable((By.CSS_SELECTOR, ".fancybox-item.fancybox-close"))
+#         )
+#         # Vérifiez si l'élément est visible
+#         if close_button.is_displayed():
+#             close_button.click()
+#             random_sleep(1, 2)  # Un petit délai après avoir fermé le pop-up
+#     except (NoSuchElementException, TimeoutException):
+#         pass  # Le pop-up n'était pas présent
